0b641abb191e9d4a53e216fde0a8915d5b159765
weeboo <phamanhduc2k2@gmail.com>
2023-11-01 22:21:36 +0700

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + TypeScript + Vite
+
+```
+Build: docker build -t react-app .
+Run: docker run -d -p 80:80 react-app
+```
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
+})
